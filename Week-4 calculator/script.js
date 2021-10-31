80d9970f13a911d7acc5758c3183f5df46ceb43c
Rahul <rahulsubramanian06@gmail.com>
2021-10-31 18:53:48 +0530

    Basic calculator has been created

---
+++ Week-4 calculator/script.js
@@ -0,0 +1,27 @@
+function add() {
+  var n1 = document.getElementById("n1").value; 
+  var n2 = document.getElementById("n2").value;
+  var sum = parseFloat(n1) + parseFloat(n2);
+  alert(sum);
+}
+
+function sub() {
+  var n1 = document.getElementById("n1").value; 
+  var n2 = document.getElementById("n2").value;
+  var sum = parseFloat(n1) - parseFloat(n2);
+  alert(sum);
+}
+
+function mul() {
+  var n1 = document.getElementById("n1").value; 
+  var n2 = document.getElementById("n2").value;
+  var sum = parseFloat(n1) * parseFloat(n2);
+  alert(sum);
+}
+
+function division() {
+  var n1 = document.getElementById("n1").value; 
+  var n2 = document.getElementById("n2").value;
+  var sum = parseFloat(n1) / parseFloat(n2);
+  alert(sum);
+}
