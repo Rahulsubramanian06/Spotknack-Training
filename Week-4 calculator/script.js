307e3b2ab109b188e7b044a55864aebad1baa5a1
Rahul <rahulsubramanian06@gmail.com>
2021-11-02 22:26:06 +0530

    commit

--- Week-4 calculator/script.js
+++ Week-4 calculator/script.js
@@ -8,20 +8,20 @@ function add() {
 function sub() {
   var n1 = document.getElementById("n1").value; 
   var n2 = document.getElementById("n2").value;
-  var sum = parseFloat(n1) - parseFloat(n2);
-  alert(sum);
+  var diff = parseFloat(n1) - parseFloat(n2);
+  alert(diff);
 }
 
 function mul() {
   var n1 = document.getElementById("n1").value; 
   var n2 = document.getElementById("n2").value;
-  var sum = parseFloat(n1) * parseFloat(n2);
-  alert(sum);
+  var prd = parseFloat(n1) * parseFloat(n2);
+  alert(prd);
 }
 
 function division() {
   var n1 = document.getElementById("n1").value; 
   var n2 = document.getElementById("n2").value;
-  var sum = parseFloat(n1) / parseFloat(n2);
-  alert(sum);
+  var qo = parseFloat(n1) / parseFloat(n2);
+  alert(qo);
 }
